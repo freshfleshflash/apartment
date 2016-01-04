0d3a50cc40cf43307c2b1147e93caba857be70df
FreshFleshFlash <pharmacystic@gmail.com>
2016-01-05 01:37:38 +0900

    svg basic

--- drawing.js
+++ drawing.js
@@ -7,18 +7,23 @@ var yGap = h / 21;
 var roomW = xGap * 4;
 var roomH = yGap * 4;
 
+var humans = [];
+
 $('#svg').svg(
-    {onLoad: initDrawing}
+    {onLoad: init}
 );
 
-function initDrawing(svg) {
+function init(svg) {
     drawBuilding(svg);
-}
+    for(var i = 0; i < 16; i++) {
+        drawHuman(svg, humans[i]);
+    }}
 
 function drawBuilding(svg) {
     for(var i = 0; i < 4; i++) {
         for(var j = 0; j < 4; j++) {
             drawRoom(svg, xGap + i * (roomW + xGap), yGap + j * (roomH + yGap));
+            humans.push(new Human(xGap + i * (roomW + xGap) + 120, yGap + j * (roomH + yGap) + 80));
         }
     }
 }
@@ -38,4 +43,18 @@ function drawRoom(svg, x, y) {
     }
 
     svg.line(x + 8*barGap, y, x + 8*barGap, y + roomH * 0.75, {stroke: 'black', strokeWidth: 1});
+}
+
+function Human(x, y) {
+    this.x = x;
+    this.y = y;
+    this.faceR = 5;
+    this.bodyW = this.faceR * 2;
+    this.bodyH = this.bodyW * 2;
+    this.legH = this.bodyH * 0.75;
+}
+
+function drawHuman(svg, h) {
+    svg.circle(h.x, h.y, h.faceR, h.faceR);
+    svg.rect(h.x - h.faceR, h.y + h.faceR, h.bodyW, h.bodyH);
 }
